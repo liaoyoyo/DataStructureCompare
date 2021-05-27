c4de165f83f7f8b825cedadec4c14525271da4e4
廖子游 <51171925+liaoyoyo@users.noreply.github.com>
2021-05-27 14:37:19 +0800

    Update LinkedList.h

--- DataStructureCompare/LinkedList.h
+++ DataStructureCompare/LinkedList.h
@@ -1,4 +1,14 @@
 #ifndef DATASTRUCTURECOMPARE_LINKEDLIST_H
 #define DATASTRUCTURECOMPARE_LINKEDLIST_H
 
+double MainLinklist(int n, char ** dataTemp, int m, char ** SearchTemp,double * creatTime,double * SearchTime);
+double LinklistCreat(int n,char * str,List * begin,List * end);
+double LinklistSearch(List * begin,char *str);
+void FreeLinklist(List * begin);
+typedef struct list List;
+
+struct list{
+    char data[parmCount+1];
+    List * next;
+};
 #endif //DATASTRUCTURECOMPARE_LINKEDLIST_H
