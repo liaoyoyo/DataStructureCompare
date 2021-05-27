a47b8ad346b1af038a97be3373aad7d87903b253
廖子游 <51171925+liaoyoyo@users.noreply.github.com>
2021-05-27 10:21:30 +0800

    Update BinarySearchTree.h

--- DataStructureCompare/BinarySearchTree.h
+++ DataStructureCompare/BinarySearchTree.h
@@ -1,7 +1,11 @@
 #ifndef DATASTRUCTURECOMPARE_BINARYSEARCHTREE_H
 #define DATASTRUCTURECOMPARE_BINARYSEARCHTREE_H
 
-
+double MainBinarySearchTree( int n, char ** dataTemp, int m, char ** SearchTemp,double * creatTime,double * SearchTime);
+double BinarySearchTreeCreat(Node **root,char *str);
+double SearchBinarySearchTree(Node * T, char * str);
+Node *NewNode(char *str);
+void FreeBinarySearchTree(Node *root);
 
 typedef struct node Node;
 typedef struct tree Tree;
