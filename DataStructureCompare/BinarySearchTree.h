10e77871510a3a78fa9ae811e7280f30fa1aafa5
廖子游 <51171925+liaoyoyo@users.noreply.github.com>
2021-05-27 10:16:31 +0800

    Update BinarySearchTree.h

--- DataStructureCompare/BinarySearchTree.h
+++ DataStructureCompare/BinarySearchTree.h
@@ -1,4 +1,14 @@
 #ifndef DATASTRUCTURECOMPARE_BINARYSEARCHTREE_H
 #define DATASTRUCTURECOMPARE_BINARYSEARCHTREE_H
 
+
+
+typedef struct node Node;
+typedef struct tree Tree;
+
+struct node {
+    char data[parmCount+1];
+    Node *left;
+    Node *right;
+};
 #endif //DATASTRUCTURECOMPARE_BINARYSEARCHTREE_H
