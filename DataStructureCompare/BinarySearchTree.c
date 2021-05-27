36e76298d4a4d7704bba817f87d8153016facf64
廖子游 <51171925+liaoyoyo@users.noreply.github.com>
2021-05-27 10:15:36 +0800

    Update BinarySearchTree.c

--- DataStructureCompare/BinarySearchTree.c
+++ DataStructureCompare/BinarySearchTree.c
@@ -5,47 +5,81 @@ typedef struct node Node;
 typedef struct tree Tree;
 
 struct node {
-    int data;
+    char data[parmCount+1];
     Node *left;
     Node *right;
 };
 
-struct tree {
-    Node *root;
-};
 
 double MainBinarySearchTree( int n, char ** dataTemp, int m, char ** SearchTemp,double * creatTime,double * SearchTime){
     double time=0;
-    
+    Node * root=NULL;
+    for(int i=0;i<n;++n){
+    	*creatTime +=	BinarySearchTreeCreat(&root,dataTemp[i]);
+    }
+    for(int i=0;i<m;++m){
+        *SearchTime += SearchBinarySearchTree(root,SearchTemp[i]);
+    }
+    //BinarySearchTreeFree(root);
     time+=*creatTime;
     time+=*SearchTime;
+    FreeBinarySearchTree(root);
     return time;
 }
 
-double BinarySearchTreeCreat(struct node tree, ElemType e) {
-    if (!T) {
-    s = new BiTNode;
-    s->data = e;
-    s->lchild = s->rchild = NULL;
-    T = s; // 被插節点*s为新的根结点
-    } else if (e.key == T->data.key)
-    return false;// 关键字等于e.key的数据元素，返回錯誤
-    if (e.key < T->data.key)
-    InsertBST(T->lchild, e);  // 將 e 插入左子樹
-    else
-    InsertBST(T->rchild, e);  // 將 e 插入右子樹
-    return true;
+double SearchBinarySearchTree(Node * T, char * str) {
+	clock_t start, finish;
+    double diff;
+    start = clock();
+    if (T==NULL) { // 查找不成功
+    	finish = clock();
+    	diff  = (double)(finish-start);
+    	return diff; 
+    } else if (strcmp(str,T->data)==0) 	{ // 查找成功
+       finish = clock();
+    	diff  = (double) (finish-start);
+    	return diff; 
+    } else if (strcmp(str,T->data)<0) // 在左子樹中繼續查找
+        return SearchBinarySearchTree(T->left,str);
+    else // 在右子樹中繼續查找
+    {
+        return SearchBinarySearchTree(T->right, str);
+}
+	finish = clock();
+    diff  = (double) (finish-start);
+    return diff;
 }
 
-double SearchBinarySearchTree(struct node T, KeyType key, BiTree f, BiTree &p) {
-    if (!T) { // 查找不成功
-        p = f;
-    return false;
-    } else if (key == T->data.key) { // 查找成功
-        p = T;
-        return true;
-    } else if (key < T->data.key) // 在左子樹中繼續查找
-        return SearchBST(T->lchild, key, T, p);
-    else // 在右子樹中繼續查找
-        return SearchBST(T->rchild, key, T, p);
+Node *NewNode(char *str){
+    Node *tempNode=malloc(sizeof(Node));
+    tempNode->left=NULL;
+    tempNode->right=NULL;
+    strcpy(tempNode->data,str);
+    return tempNode;
+}
+
+double BinarySearchTreeCreat(Node **root,char *str){
+    clock_t start, finish;
+    double diff;
+    start = clock();
+    Node **ptr=root;
+    Node *t;
+    while((t=*ptr)!=NULL){
+        if(strcmp(str,t->data)>=0){
+            ptr=&t->right;
+        }else if(strcmp(str,t->data)<0){
+            ptr=&t->left;
+        }
+    }
+    *ptr=NewNode(str);
+    finish = clock();
+    diff  = (double) (finish-start);
+    return diff;
+}
+
+void FreeBinarySearchTree(Node *root){
+    if(root==NULL) return;
+    FreeBinarySearchTree(root->left);
+    free(root);
+    FreeBinarySearchTree(root->right);
 }
