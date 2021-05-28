8a5221715819890887e63f22b6db7a495c639123
廖子游 <51171925+liaoyoyo@users.noreply.github.com>
2021-05-28 13:29:15 +0800

    Update Hash.h

--- DataStructureCompare/Hash.h
+++ DataStructureCompare/Hash.h
@@ -1,4 +1,6 @@
 #ifndef DATASTRUCTURECOMPARE_HASH_H
 #define DATASTRUCTURECOMPARE_HASH_H
-
+double MainHash(int n, char ** dataTemp, int m, char ** SearchTemp,double * creatTime,double * SearchTime);
+double HashCreat(int n, char ** dataTemp,char ** ArrayData);
+double HashSearch(int m, char ** SearchTemp);
 #endif //DATASTRUCTURECOMPARE_HASH_H
